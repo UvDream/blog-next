e56761838705f137a47442126e9ad3872ccf0228
wangzhongjie <UvDream@163.com>
2020-03-07 16:25:39 +0800

    perf👌 html

---
+++ pages/_document.js
@@ -0,0 +1,27 @@
+import Document, { Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return { ...initialProps }
+  }
+
+  render() {
+    return (
+      <html lang="zh">
+        <Head>
+        <title>汪中杰的博客</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta
+          name="description"
+          content="首页 | 汪中杰的技术博客,让技术产生价值!"
+        />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
+  }
+}

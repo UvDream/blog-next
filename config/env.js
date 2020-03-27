071d6cc2697d18fdca6802ff1cce75ef9c087150
wangzhongjie <uvdream@163.com>
2020-03-27 16:40:50 +0800

    fix🐛 环境更改

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2019-10-09 16:23:07
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2020-03-19 20:27:24
+ * @LastEditTime: 2020-03-26 15:21:53
  * @Description:url
  * @Email: UvDream@163.com
  */
@@ -10,8 +10,8 @@
 let baseUrl = "";
 let imgUrl = "";
 if (process.env.NODE_ENV == "development") {
-   baseUrl = `http://118.25.110.93:8000`; //开发环境地址
-  //baseUrl = `http://localhost:8000`; //开发环境地址
+   //baseUrl = `http://118.25.110.93:8000`; //开发环境地址
+  baseUrl = `http://localhost:8000`; //开发环境地址
   imgUrl=`http://localhost:3001`
 } else if (process.env.NODE_ENV == "production") {
   baseUrl = `http://118.25.110.93:8000`; //生产环境地址

2b17a223f4aa4c880faa23507e523df95d5ab9c9
wangzhongjie <uvdream@163.com>
2020-03-31 18:55:25 +0800

    feat✨ 深色模式

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2019-10-09 16:23:07
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2020-03-31 15:54:17
+ * @LastEditTime: 2020-03-31 18:54:41
  * @Description:url
  * @Email: UvDream@163.com
  */
@@ -20,7 +20,7 @@ if (process.env.NODE_ENV == "development") {
   adminUrl=`http://118.25.110.93:4200`
 }
 // 暗黑主题
-if(dayjs().hour()>8 && dayjs().hour()<=18){
+if(dayjs().hour()>18 ){
   Color.bgColor="#fff" ,Color.textColor="#000",Color.otherColor="#F5F5F5"
 }else{
   Color.bgColor="#000" ,Color.textColor="#fff",Color.otherColor="#000"

80367c91d7c13767e7a3f4063a031cc4eaf3e476
wangzhongjie <uvdream@163.com>
2020-06-19 10:37:29 +0800

    feat✨ 提交一个版本

--- config/env.js
+++ config/env.js
@@ -2,7 +2,7 @@
  * @Author: wangzhongjie
  * @Date: 2019-10-09 16:23:07
  * @LastEditors: wangzhongjie
- * @LastEditTime: 2020-04-23 16:05:01
+ * @LastEditTime: 2020-06-19 10:33:44
  * @Description:url
  * @Email: UvDream@163.com
  */
@@ -11,8 +11,8 @@ let baseUrl = "",adminUrl="";
 let iconfontVersion = ["1753589_lp9rqr3u89"];
 let iconfontUrl = `//at.alicdn.com/t/font_$key.css`;
 if (process.env.NODE_ENV == "development") {
-   baseUrl = `http://127.0.0.1:8000`; //开发环境地址
-  // baseUrl = `https://www.uvdream.cn/api/`; //开发环境地址
+  // baseUrl = `http://127.0.0.1:8000`; //开发环境地址
+   baseUrl = `https://www.uvdream.cn/api/`; //开发环境地址
 } else if (process.env.NODE_ENV == "production") {
   baseUrl = `https://www.uvdream.cn/api/`; //生产环境地址
   adminUrl=`https://www.uvdream.cn:4200`
--- pages/articleDetail.jsx
+++ pages/articleDetail.jsx
@@ -1,22 +1,71 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Head from "next/head";
 import Header from "../components/Header";
-import { Row, Col, Icon, Affix, message } from "antd";
+import { Row, Col, Icon, Affix, BackTop, message } from "antd";
 import Author from "../components/Author";
 import Article from "../api/article/index";
+import Tocify from "../components/tocify.tsx";
 import Footer from "../components/Footer";
+import marked from "marked";
+import hljs from "highlight.js";
 import dayjs from "dayjs";
-import dynamic from "next/dynamic";
-import ChangeTheme from "../components/changeTheme";
 
-const Vditor = dynamic(import("../components/vditor"), {
-  ssr: false,
-});
+import "../static/style/pages/theme.less";
 import "../static/style/pages/article-detail.less";
 
 const ArticleDetail = (props) => {
+  useEffect(() => {
+    let copyText = document.getElementsByClassName("copy");
+    for (let i = 0; i < copyText.length; i++) {
+      copyText[i].onclick = (e) => {
+        //console.log(e.target);
+        var oInput = document.createElement("textarea");
+        oInput.value = e.target.children[0].innerHTML;
+        document.body.appendChild(oInput);
+        oInput.select();
+        document.execCommand("Copy");
+        oInput.className = "oInput";
+        oInput.style.display = "none";
+        message.success("复制代码成功!");
+      };
+    }
+  }, []);
+
+  const tocify = new Tocify();
+  const renderer = new marked.Renderer();
+  marked.setOptions({
+    renderer: renderer,
+    gfm: true,
+    pedantic: false,
+    sanitize: false,
+    tables: true,
+    breaks: false,
+    smartLists: true,
+    smartypants: false,
+    highlight: function (code) {
+      return hljs.highlightAuto(code).value;
+    },
+  });
+  renderer.heading = function (text, level, raw) {
+    const anchor = tocify.add(text, level);
+    return `<a id="${anchor}" href="#${anchor}" class="anchor-fix"><h${level}>${text}</h${level}></a>\n`;
+  };
+  // 重写代码块
+  renderer.code = (code, language, isEscaped) => {
+    return (
+      '<pre class="hljs" ><span  id="copy" class="copy">复制代码<span style="display: none;">' +
+      code +
+      '</span></span><code  class="${language}"  >' +
+      hljs.highlightAuto(code).value +
+      "</code></pre>"
+    );
+  };
+
+  let html = marked(props.article_content);
+
   return (
     <div className="detail">
+      <BackTop />
       <Head>
         <title>汪中杰的博客-{props.title}</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
@@ -26,16 +75,8 @@ const ArticleDetail = (props) => {
         />
       </Head>
       <Header></Header>
-      <Row className="detail-content" type="flex" justify="center">
-        <Col
-          className=" detail-content-left"
-          xs={24}
-          sm={24}
-          md={16}
-          lg={18}
-          xl={14}
-        >
-          <ChangeTheme />
+      <Row className="common-main" type="flex" justify="center">
+        <Col className=" common-left" xs={24} sm={24} md={16} lg={18} xl={14}>
           {/* 标题 */}
           <div className="detail-title">{props.title}</div>
           {/* tag */}
@@ -55,10 +96,21 @@ const ArticleDetail = (props) => {
               {props.user_name}
             </span>
           </div>
-          <Vditor mdContent={props.article_content} />
+          {/* 内容 */}
+          <div
+            className="detail-content"
+            dangerouslySetInnerHTML={{ __html: html }}
+          ></div>
         </Col>
         <Col className="common-right" xs={0} sm={0} md={7} lg={5} xl={4}>
           <Author />
+          <Affix offsetTop={60}>
+            <div className="detail-nav common-box">
+              <div className="detail-nav-title">文章目录</div>
+              {/* <MarkNav className="article-menu" source={html} ordered={false} /> */}
+              {tocify && tocify.render()}
+            </div>
+          </Affix>
         </Col>
       </Row>
       <Footer />
@@ -69,6 +121,8 @@ ArticleDetail.getInitialProps = async (context) => {
   let id = context.query.id;
   const promise = new Promise((resolve) => {
     Article.detail({ id: id }).then((res) => {
+      //console.log("获取文章");
+      //console.log(res);
       resolve(res.data);
     });
   });
